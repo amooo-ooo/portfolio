331bb03b3d97ea48a3a061021fe3fcebfbd78b73
amoooooo <amor.budiyanto2006@gmail.com>
2024-06-23 14:49:42 +1200

    base

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script>
 	import '$lib/styles/style.scss';
+	import Conway from './Conway.svelte';
+	let bored = false;
 </script>
 
 <section>
 	<div class="content">
 		<p>
-			Hi, I'm Amor Budiyanto. I'm a Year 13 High School student at Cashmere High School with passion in Computer Science. 
+			Hi, I'm Amor Budiyanto. I'm a Year 13 High School student at Cashmere High School with passion
+			in Computer Science.
 		</p>
 	</div>
-	<div class="board">
-		<p>bored?</p>
+	<div class="board {bored ? 'game' : ''}" on:click={() => (bored = true)}>
+		{#if bored}
+			<Conway></Conway>
+		{:else}
+			<p>bored?</p>
+		{/if}
 	</div>
 </section>
--- client/src/routes/Works.svelte
+++ client/src/routes/Works.svelte
@@ -1,29 +1,48 @@
 <script>
-	import NavFade from "./NavFade.svelte";
+	import NavFade from './NavFade.svelte';
 
-    const roles = [{"title": "Digital Technologies Student Prefect", "subtitle":"Cashmere High School", "year": "2024"},
-    {"title": "UC STAR Student", "subtitle": "University of Canterbury", "year": "2024"},
-        {"title": "Youth Leader", "subtitle": "New Zealand National Refugee Youth Coucil", "year": "2024"},
-        {"title": "Chief Technology Officer", "subtitle": "Ōtautahi Charity Ball", "year": "2024"},
-        {"title": "Full Stack Developer", "subtitle": "Self-Employed", "year": "2022"}
-    ]
+	export const field = 'roles';
+
+	const infos = {
+		roles: [
+			{
+				title: 'Digital Technologies Student Prefect',
+				subtitle: 'Cashmere High School',
+				year: '2024'
+			},
+			{ title: 'UC STAR Student', subtitle: 'University of Canterbury', year: '2024' },
+			{
+				title: 'Youth Leader',
+				subtitle: 'New Zealand National Refugee Youth Coucil',
+				year: '2024'
+			},
+			{ title: 'Chief Technology Officer', subtitle: 'Ōtautahi Charity Ball', year: '2024' },
+			{ title: 'Full Stack Developer', subtitle: 'Self-Employed', year: '2022' }
+		],
+        projects: [
+            {title: "Charity Website", subtitle: "Hoon Hay Foodbank"},
+            {title: "Selectify", subtitle: "Ōtautahi Charity Ball"},
+            {title: "PEP8PLUS", subtitle: "Opensource Project"}, 
+            {title: "Charity Website", subtitle: "Ōtautahi Charity Ball"},
+        ]
+	};
 </script>
 
 <div class="main">
-    <div class="controls">
-        <div>
-            <p>Home</p>
-            <p>Roles[{roles.length}]</p>
-        </div>
+	<div class="controls">
+		<div>
+			<p>Home</p>
+			<p>Roles[{infos[field].length}]</p>
+		</div>
 
-        <div>
-            <p>About</p>
-            <p>Contact</p>
-        </div>
-    </div>
-    <div class="roles"> 
-        {#each roles as role, index}
-        <NavFade title={role.title} subtitle={role.subtitle} index={index} />
-        {/each}
-    </div>
+		<div>
+			<p>About</p>
+			<p>Contact</p>
+		</div>
+	</div>
+	<div class="roles">
+		{#each infos[field] as role, index}
+			<NavFade title={role.title} subtitle={role.subtitle} {index} />
+		{/each}
+	</div>
 </div>
